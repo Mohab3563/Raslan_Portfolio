b3ecdea3e56040e9afcbe03881718b8bb01bc738
MOHAB <mohab3563@gmail.com>
2026-05-24 07:06:45 -0700

    update website

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ interface NavbarProps {
 }
 
 const links = [
-  { label: 'Hom', href: '#home' },
+  { label: 'Home', href: '#home' },
   { label: 'About', href: '#about' },
   { label: 'Projects', href: '#projects' },
   { label: 'Contact', href: '#contact' },
